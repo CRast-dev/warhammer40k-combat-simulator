e26942fbdbcfab5e40ed0ae7386710d00b3c0a27
Christian Rast <christian.rast98@gmail.com>
2026-08-19 16:14:33 +0200

    checkpoint before springboot

--- src/test/java/combat/AllocationGroupTest.java
+++ src/test/java/combat/AllocationGroupTest.java
@@ -10,25 +10,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class AllocationGroupTest {
     @Test
     void modelsWithSameCharacteristicsAreInSameGroup() {
+        // Arrange
         List<Model> testModels = new ArrayList<>();
         testModels.add(new Model(4,3,0,2,0,null));
         testModels.add(new Model(4,3,0,2,0,null));
         testModels.add(new Model(4,3,0,2,0,null));
         Unit unit = new Unit("Test Model", testModels);
+        // Act
         List<AllocationGroup> groups = AllocationGroup.initializeAllocationGroups(unit);
-
+        // Assert
         assertEquals(1, groups.size());
         assertEquals(3, groups.get(0).getModels().size());
     }
     @Test
     void modelsWithDifferentCharacteristicsAreInDifferentGroup() {
+        // Arrange
         List<Model> testModels = new ArrayList<>();
         testModels.add(new Model(4,3,0,2,0,null));
         testModels.add(new Model(4,5,0,2,0,null));
         testModels.add(new Model(4,0,0,2,0,null));
         Unit unit = new Unit("Test Model", testModels);
+        // Act
         List<AllocationGroup> groups = AllocationGroup.initializeAllocationGroups(unit);
-
+        // Assert
         assertEquals(3, groups.size());
     }
 
